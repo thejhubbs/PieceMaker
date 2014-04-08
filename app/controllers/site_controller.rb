33c9f90f0711d4bc40b65048245576f55ffd59d5
Jordan Hubbard <thejhubbs@gmail.com>
2014-04-08 19:37:08 -0400

    D

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,9 @@
 class SiteController < ApplicationController
   def index
   end
+
+  def page
+    name = params[:name].split('-').collect { |x| x.titlecase }.join(' ')
+    @page = Clientalk::Page.where(:name => name).first
+  end
 end
